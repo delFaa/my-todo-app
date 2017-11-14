e5ff77d4554ed083d3ae47a5d5fb973d1973b6d5
Elisa della faille <elisadellafaille@MacBook-Pro-de-Elisa.local>
2017-11-14 19:45:38 +0100

    good positioning of page toggle

--- app/todo.js
+++ app/todo.js
@@ -1,35 +1,41 @@
 "use strict";
-// create date picker in edit
-// var config = {
-// 	target: 'start-picker'
-// };
-// var myDatepicker = new MtrDatepicker(config);
-//
-// var config = {
-// 	target: 'end-picker'
-// };
-// var myDatepicker = new MtrDatepicker(config);
 
 // pass from main to edit
 
+var editContainer=document.getElementById('goToEdit');
 
-
-
-// console.log(html);
-document.getElementById('goToEdit').addEventListener('click',
+editContainer.addEventListener('click',
 	function (){
-		console.log('mdr');
 		if (this.id == 'goToEdit'){
 			document.getElementById('edit-container')		.style.left = '0';
+			document.getElementById('edit-container')		.style.right= '0';
 			document.getElementById('main-container')		.style.left= '-100%';
-			console.log('haha');
+			document.getElementById('edit-footer')			.style.left = '0';
+			document.getElementById('edit-footer')			.style.right = '0';
+
 			this.id = 'goToMain';
 		}
 		else{
-			document.getElementById('edit-container')		.style.left = '100%';
 			document.getElementById('main-container')		.style.left = '0';
-			console.log('lol');
+			document.getElementById('main-container')		.style.right = '0';
+			document.getElementById('edit-container')		.style.left = '100%';
+			document.getElementById('edit-container')		.style.right= '0';
+			document.getElementById('main-footer')			.style.left = '0';
+			document.getElementById('main-footer')			.style.right = '0';
+
+
 			this.id = 'goToEdit';
 		}
 	}
 )
+
+// create date picker in edit
+var config = {
+	target: 'start-picker'
+};
+var myDatepicker = new MtrDatepicker(config);
+
+var config = {
+	target: 'end-picker'
+};
+var myDatepicker = new MtrDatepicker(config);
